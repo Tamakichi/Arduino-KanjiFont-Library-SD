00138d39302008b49b011c1f68bf659cf31e013a
たま吉さん(Tamakichi-San) <tamacat2014@gmail.com>
2018-07-11 19:01:24 +0900

    サンプルスケッチの差し換え

--- libraries/sdfonts/exsample/test_sdfontsTFT_STM32/test_sdfontsTFT_STM32.ino
+++ libraries/sdfonts/exsample/test_sdfontsTFT_STM32/test_sdfontsTFT_STM32.ino
@@ -50,6 +50,20 @@ XPT2046_touch ts(TS_CS, SPI_2); // Chip Select pin, SPI port
 SPIClass  SPI_1(1);
 Adafruit_ILI9341_STM tft = Adafruit_ILI9341_STM(TFT_CS, TFT_DC, TFT_RST,SPI_1);
 
+// スクロールアップ
+void scrollUp(uint16_t y0,uint16_t h,uint16_t bg) {
+  uint16_t sc_w = tft.width();
+  uint16_t sc_h = tft.height();
+  uint16_t buf[sc_w];
+
+  for (uint16_t y=h+y0; y < sc_h; y++) {  
+    tft.readPixels(0, y, sc_w-1, y, buf);
+    tft.setAddrWindow(0, y-h, sc_w-1, y-h);
+    tft.pushColors(buf, sc_w, 0);
+  }
+  tft.fillRect(0, sc_h-h, sc_w-1, h, bg);
+}
+
 // 指定位置に１文字表示
 void mputc(uint16_t x, uint16_t y, uint8_t* buf, uint16_t fg, uint16_t bg) {   
   uint16_t w = SDfonts.getWidth();
@@ -70,7 +84,7 @@ void mputc(uint16_t x, uint16_t y, uint8_t* buf, uint16_t fg, uint16_t bg) {
 // 指定位置に文字列表示
 void mprint(uint16_t x, uint16_t y, char* str, uint16_t fg, uint16_t bg) { 
   uint8_t buf[MAXFONTLEN]; 
-  int16_t   len,x0 = x;
+  int16_t   len,x0 = x, y0 = y;
   char* pUTF8 = str;
 
   SDfonts.open();   // フォントのオープン
@@ -83,7 +97,7 @@ void mprint(uint16_t x, uint16_t y, char* str, uint16_t fg, uint16_t bg) {
       if (y + SDfonts.getHeight()*2 < tft.height()) {
         y += SDfonts.getHeight()+2;
       } else {  
-        break;
+        scrollUp(y0, SDfonts.getHeight()+2, bg);
       }
     }
   }      
@@ -115,11 +129,11 @@ static const char* text =
    "どこで生れたかとんと見当がつかぬ。"
    "何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。"
    "吾輩はここで始めて人間というものを見た。"
-   "しかもあとで聞くとそれは書生という人間中で一番獰悪どうあくな種族であったそうだ。"
+   "しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。"
    "というのは時々我々を捕つかまえて煮にて食うという話である。"
    "しかしその当時は何という考もなかったから別段恐しいとも思わなかった。"
-   "ただ彼の掌てのひらに載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。"
-   "掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始みはじめであろう。"
+   "ただ彼の掌のひらに載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。"
+   "掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始めであろう。"
  ;
 
 void loop() {
@@ -128,7 +142,11 @@ void loop() {
     tft.fillScreen(ILI9341_BLACK);
     SDfonts.setFontSizeAsIndex(i);
     mprint(2, 2, (char*)text, ILI9341_WHITE, ILI9341_BLACK);
-
+/*
+    for (uint8_t j=0; j < 5; j++) {
+      scrollUp(2,SDfonts.getHeight()+2,ILI9341_BLACK);
+    }
+*/
     // テキスト表示後、3秒間タッチスクリーンを使って描画出来る
     // (SDfontsとタッチスクリーンのSPIバス共有利用の動作確認)
     uint32_t t = millis() + 3000;
